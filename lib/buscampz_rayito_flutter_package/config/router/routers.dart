a3cfa3722f46e57f36bb6b528ecb445009671456
Jisus <jesusdev2407@gmail.com>
2024-06-18 11:39:41 -0500

    implement-router-provider-folders-listview-screen

---
+++ lib/buscampz_rayito_flutter_package/config/router/routers.dart
@@ -0,0 +1,21 @@
+import 'package:buscampz_flutter/buscampz_rayito_flutter_package/example/example_screens/example_screens.dart';
+import 'package:buscampz_flutter/buscampz_rayito_flutter_package/example/example_screens/list_components_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouterProvider = Provider<GoRouter>(
+  (ref) {
+    return GoRouter(
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (context, state) => const ListComponentsScreen(),
+        ),
+        GoRoute(
+          path: '/example',
+          builder: (context, state) => const ExampleScreen(),
+        ),
+      ],
+    );
+  },
+);
